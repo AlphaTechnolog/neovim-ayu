90c840bc34d93d83e4af6624adb92897a56dca13
Gabriel Guerra <gfranklings@gmail.com>
2026-05-21 16:56:49 -0400

    chore: changes again

--- lua/lualine/themes/ayu.lua
+++ lua/lualine/themes/ayu.lua
@@ -3,33 +3,33 @@ local colors = require('ayu.colors')
 local ayu = {
   visual = {
     a = { fg = colors.bg, bg = colors.accent, gui = 'bold' },
-    b = { fg = colors.accent, bg = colors.panel_border },
+    b = { fg = colors.accent, bg = colors.bg },
   },
   replace = {
     a = { fg = colors.bg, bg = colors.markup, gui = 'bold' },
-    b = { fg = colors.markup, bg = colors.panel_border },
+    b = { fg = colors.markup, bg = colors.bg },
   },
   inactive = {
-    a = { fg = colors.fg, bg = colors.panel_border, gui = 'bold' },
-    b = { fg = colors.fg, bg = colors.panel_border },
-    c = { fg = colors.fg, bg = colors.panel_border },
+    a = { fg = colors.fg, bg = colors.bg, gui = 'bold' },
+    b = { fg = colors.fg, bg = colors.bg },
+    c = { fg = colors.fg, bg = colors.bg },
   },
   normal = {
     a = { fg = colors.bg, bg = colors.entity, gui = 'bold' },
-    b = { fg = colors.entity, bg = colors.panel_border },
-    c = { fg = colors.fg, bg = colors.panel_border },
+    b = { fg = colors.entity, bg = colors.bg },
+    c = { fg = colors.fg, bg = colors.bg },
   },
   insert = {
     a = { fg = colors.bg, bg = colors.string, gui = 'bold' },
-    b = { fg = colors.string, bg = colors.panel_border },
+    b = { fg = colors.string, bg = colors.bg },
   },
   command = {
     a = { fg = colors.bg, bg = colors.constant, gui = 'bold' },
-    b = { fg = colors.constant, bg = colors.panel_border },
+    b = { fg = colors.constant, bg = colors.bg },
   },
   terminal = {
     a = { fg = colors.bg, bg = colors.string, gui = 'bold' },
-    b = { fg = colors.string, bg = colors.panel_border },
+    b = { fg = colors.string, bg = colors.bg },
   },
 }
 
